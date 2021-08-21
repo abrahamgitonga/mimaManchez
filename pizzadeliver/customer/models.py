d7e66b20bfd93433ad82743d15f4440575d6ee65
abraham <abrahamgitonga98@gmail.com>
2021-08-21 10:06:08 +0300

    mima manchez

--- pizzadeliver/customer/models.py
+++ pizzadeliver/customer/models.py
@@ -22,6 +22,12 @@ class OrderModel(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     items = models.ManyToManyField('MenuItem', related_name='order', blank=True)
+    name = models.CharField(max_length=50, blank=True)
+    email = models.CharField(max_length=50, blank=True)
+    street = models.CharField(max_length=50, blank=True)
+    county = models.CharField(max_length=50, blank=True)
+    town = models.CharField(max_length=15, blank=True)
+    zip_code = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return f'Order: {self.created_on.strftime("%b %d %I: %M %p")}'
--- pizzadeliver/pizzadeliver/settings.py
+++ pizzadeliver/pizzadeliver/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+from decouple import config,Csv
 from pathlib import Path
 
 
@@ -22,12 +23,35 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3r)w4d73!+7go#pa+^x5-kv@@7j6frg#433f39@-r4_2+v9!z='
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
+# development
+# if config('MODE')=="dev":
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#            'NAME': config('DB_NAME'),
+#            'USER': config('DB_USER'),
+#            'PASSWORD': config('DB_PASSWORD'),
+#            'HOST': config('DB_HOST'),
+#            'PORT': '',
+#        }
+       
+#    }# production
+# else:
+#    DATABASES = {
+#        'default': dj_database_url.config(
+#            default=config('DATABASE_URL')
+#        )
+#    }
 
-ALLOWED_HOSTS = []
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
 
 
 # Application definition
